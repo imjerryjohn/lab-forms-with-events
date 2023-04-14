d5f121ac100f5d7d61da6001e936e3f5b7b74efc
Jerry John <jerryjohn@jerrys-air.myfiosgateway.com>
2023-04-14 19:14:46 -0400

    Final Commit

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 console.log("Code your solution!")
+let header = document.querySelector("h1")
+header.textContent = "My To-Do's List"
+const form = document.querySelector("form")
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
+ 
+let unOrdered = document.querySelector("ul")
+let list = document.createElement("li")
+let listText = event.target.list.value
+
+if(!listText){
+    let errorMessage = document.createElement("p")
+    errorMessage.textContent = "Please Try Again Later"
+       form.append(errorMessage)
+}
+else {
+list.textContent = listText
+console.log(listText)
+unOrdered.append(list)
+
+let removedMessages = document.querySelectorAll("p")
+removedMessages.forEach(message => message.remove())
+
+}
+ list.addEventListener("click", () => {
+   list.style.textDecoration = "line-through"
+ })
+
+})
